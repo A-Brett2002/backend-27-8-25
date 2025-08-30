b5e04ec565e9099070995edb84ec373694740d5a
A-Brett2002 <alorenzom2@miumg.edu.gt>
2025-08-30 12:47:02 -0600

    committed

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,7 +1,7 @@
 const querystring = require('querystring');
 const index=(req, res) => {
     const query=querystring.stringify(req.query);
-    fetch(`https://fakestoreapi.com/users:?${query}`)
+    fetch(`https://fakestoreapi.com/users?`+query)
     .then(response => response.json())
     .then(usuarios => {
         res.render(`usuarios`, {usuarios})
