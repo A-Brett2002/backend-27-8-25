998bc7d4182fa46f42bcb395ded6c9f2414a546f
A-Brett2002 <alorenzom2@miumg.edu.gt>
2025-08-30 19:58:35 -0600

    committed

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.set('view engine', 'ejs');
 app.set(`views`, path.join(__dirname,`src/views`));
 const mainRouter = require('./src/routes/main.routes');
 app.use('/', mainRouter);
+
 app.use(`/productos`, require('./src/routes/productos.router'));
 app.use(`/usuarios`, require('./src/routes/usuarios.router'));
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -6,9 +6,6 @@ const index=(req, res) => {
     .then(usuarios => {
         res.render(`usuarios`, {usuarios})
     });
-    fetch('https://fakestoreapi.com/users?'+query)
-  .then(response => response.json())
-  .then(data => console.log(data));
 }
 
 module.exports={
