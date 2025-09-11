8dbffa2884a04dcda1c5bf29313d8e704aba1a98
A-Brett2002 <alorenzom2@miumg.edu.gt>
2025-09-11 02:51:53 -0600

    committed

--- src/routes/categorias.router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router =  express.Router();
-const Controller = require('../controllers/categorias.controller');
-router.get('/',Controller.index);
-router.get('/:id',Controller.show);
-module.exports = router;
